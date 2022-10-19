5fa31f7fe2c01159f0700ae81bc2b2cce6d7967d
Admin <bskaya.mert@gmail.com>
2022-10-20 01:26:34 +0300

    Sentry, Mocha and Chai were added

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 var createError = require('http-errors');
 var express = require('express');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 const expressLayouts = require('express-ejs-layouts')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 var app = express();
+
+// Sentry initialization
+Sentry.init({ 
+  dsn: "https://afaae32b3c3446b5a401e0bcc7330f70@o4503987293061120.ingest.sentry.io/4503987296403458",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // setting enverionmental variables
 dotenv.config();
@@ -29,6 +50,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,6 +61,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+//Sentry error handler
+app.use(Sentry.Handlers.errorHandler());
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -46,7 +72,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render(res.sentry + "\n");
 });
 
 module.exports = app;

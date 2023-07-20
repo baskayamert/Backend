f1d2b2928221146f7bf3bb9f9a8dd2374dd9bbf7
baskayamert <48298106+baskayamert@users.noreply.github.com>
2023-07-21 00:50:57 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,16 +2,16 @@ const axios =  require('axios');
 
 module.exports = {
     getCategoriesByParentId: async (parentId) => {
-        const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/categories/parent/${parentId}?secretKey=${process.env.API_KEY}`)
+        const response = await axios.get(`${process.env.API_URL}/categories/parent/${parentId}?secretKey=${process.env.API_KEY}`)
         return response.data 
     },
     getCategoriesById: async (id) => {
-        const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/categories/${id}?secretKey=${process.env.API_KEY}`)
+        const response = await axios.get(`${process.env.API_URL}/categories/${id}?secretKey=${process.env.API_KEY}`)
         return response.data 
     },
     getProductsByCategoryId: async (id, page) => {
         try {
-            const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/products/product_search?secretKey=${process.env.API_KEY}&primary_category_id=${id}&page=${page}`)
+            const response = await axios.get(`${process.env.API_URL}/products/product_search?secretKey=${process.env.API_KEY}&primary_category_id=${id}&page=${page}`)
             return response.data 
         }catch(err){
             return undefined
@@ -19,7 +19,7 @@ module.exports = {
         
     },
     getProductById: async (id) => {
-        const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/products/product_search?secretKey=${process.env.API_KEY}&id=${id}`)
+        const response = await axios.get(`${process.env.API_URL}/products/product_search?secretKey=${process.env.API_KEY}&id=${id}`)
         return response.data 
     },
     getItems: async (items) => {
@@ -33,7 +33,7 @@ module.exports = {
                 }
             }
             if(!doesProductExist){
-                const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/products/product_search?secretKey=${process.env.API_KEY}&id=${item.productId}`)
+                const response = await axios.get(`${process.env.API_URL}/products/product_search?secretKey=${process.env.API_KEY}&id=${item.productId}`)
                 products.push(response.data)
             }
             
@@ -41,11 +41,11 @@ module.exports = {
         return products
     },
     signUp: async (newUser) => {
-        const response = await axios.post(`https://backend-academy-osf.herokuapp.com/api/auth/signup`, newUser)
+        const response = await axios.post(`${process.env.API_URL}/auth/signup`, newUser)
         return response.data
     },
     signIn: async (loggedInUser) => {
-        const response = await axios.post(`https://backend-academy-osf.herokuapp.com/api/auth/signin`, loggedInUser)
+        const response = await axios.post(`${process.env.API_URL}/auth/signin`, loggedInUser)
         return response.data
     },
     getCart: async (jwt) => {
@@ -54,7 +54,7 @@ module.exports = {
                 Authorization: jwt
             }
         })
-        const response = await axiosRequest.get(`https://backend-academy-osf.herokuapp.com/api/cart?secretKey=${process.env.API_KEY}`)
+        const response = await axiosRequest.get(`${process.env.API_URL}/cart?secretKey=${process.env.API_KEY}`)
         return response.data
     },
     addItemToCart: async (jwt, product) => {
@@ -64,11 +64,11 @@ module.exports = {
             }
         })
 
-        const response = await axiosRequest.post(`https://backend-academy-osf.herokuapp.com/api/cart/addItem`, product)
+        const response = await axiosRequest.post(`${process.env.API_URL}/cart/addItem`, product)
         return response.data
     },
     removeItemFromCart: async (jwt, product) => {
-        const url = 'https://backend-academy-osf.herokuapp.com/api/cart/removeItem'
+        const url = '${process.env.API_URL}/cart/removeItem'
         const config = {
           headers: { Authorization: jwt },
           url,
@@ -80,7 +80,7 @@ module.exports = {
         return response.data   
     },
     cleanCart: async (jwt, cart) => {
-        const url = 'https://backend-academy-osf.herokuapp.com/api/cart/removeItem'
+        const url = '${process.env.API_URL}/cart/removeItem'
         for(item of cart.items){ 
             const product = {
                 secretKey: process.env.API_KEY,
@@ -101,7 +101,7 @@ module.exports = {
         let totalProducts = []
         while(true){
             try{
-                const response = await axios.get(`https://backend-academy-osf.herokuapp.com/api/products/product_search?secretKey=${process.env.API_KEY}&page=${page}`)
+                const response = await axios.get(`${process.env.API_URL}/products/product_search?secretKey=${process.env.API_KEY}&page=${page}`)
                 totalProducts.push(...response.data)
                 page++
             }catch(err){
@@ -118,7 +118,7 @@ module.exports = {
                 Authorization: jwt
             }
         })
-        const response = await axiosRequest.get(`https://backend-academy-osf.herokuapp.com/api/wishlist?secretKey=${process.env.API_KEY}`)
+        const response = await axiosRequest.get(`${process.env.API_URL}/wishlist?secretKey=${process.env.API_KEY}`)
 
         return response.data
     },
@@ -128,11 +128,11 @@ module.exports = {
                 Authorization: jwt
             }
         })
-        const response = await axiosRequest.post(`https://backend-academy-osf.herokuapp.com/api/wishlist/addItem`, item)
+        const response = await axiosRequest.post(`${process.env.API_URL}/wishlist/addItem`, item)
         return response.data
     },
     removeItemFromWishList: async (jwt, item) => {
-        const url = 'https://backend-academy-osf.herokuapp.com/api/wishlist/removeItem'
+        const url = '${process.env.API_URL}/wishlist/removeItem'
         const config = {
           headers: { Authorization: jwt },
           url,
@@ -149,7 +149,7 @@ module.exports = {
                 Authorization: jwt
             }
         })
-        const response = await axiosRequest.post(`https://backend-academy-osf.herokuapp.com/api/cart/changeItemQuantity`, item)
+        const response = await axiosRequest.post(`${process.env.API_URL}/cart/changeItemQuantity`, item)
         return response.data
     },
     changeItemQuantityForWishList: async (jwt, item) => {
@@ -158,7 +158,7 @@ module.exports = {
                 Authorization: jwt
             }
         })
-        const response = await axiosRequest.post(`https://backend-academy-osf.herokuapp.com/api/wishlist/changeItemQuantity`, item)
+        const response = await axiosRequest.post(`${process.env.API_URL}/wishlist/changeItemQuantity`, item)
         return response.data
     }
 }
